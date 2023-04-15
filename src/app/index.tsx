cc8974f2591651888aaff9e6eeceb0ceeb16599c
Omar Díaz <omardh1xxx@gmail.com>
2023-04-15 13:14:21 -0600

    curso terminado

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { App } from './containers/App';
-import './assets/favicon.ico'
+import React from "react";
+import { hydrateRoot } from "react-dom/client";
+import { App } from "./containers/App";
+import "./assets/favicon.ico";
+import { BrowserRouter } from "react-router-dom";
 
-const container = document.getElementById('app');
-const root = createRoot(container)
 
-root.render(<App />)
+const container = document.getElementById("app");
+
+const initialProps = window.__INITIAL_PROPS__ 
+console.log(initialProps)
+
+hydrateRoot(
+  container,
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
